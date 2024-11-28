57a30af1f418d28d2e9b75af49f87fd71735d4b6
hsscode <harshsengar28@gmail.com>
2024-11-28 22:20:38 +0530

    solution 26  # https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=problem-list-v2&envId=array&difficulty=EASY

--- solution26.py
+++ solution26.py
@@ -6,23 +6,17 @@
 nums = [0,0,1,1,1,2,2,3,3,4]
 
 # Output: [1,2]
-# logic:  we hqave to remove duplicate 
+# logic:  we have to remove duplicate without using extra space , so we compare 2 numbers and move forward
+# coz they are in ascending  
 
-l=1
 
-for r in range(1,len(nums)-1):
-    if nums[r] != nums[r+1]:
-        nums[l]=nums[r+1]
-        l=l+1
+l=1  # Start the  pointer at index 1
+
+for r in range(1,len(nums)):   #pointer starts from index 1
+    if nums[r] != nums[r-1]:  # Compare current number with the previous one
+        nums[l]=nums[r]
+        l=l+1   # Move the slow pointer
 
 print(nums)
 
-
-
-# for r in range(1,len(nums)):
-#     if nums[r] != nums[r-1]:
-#         nums[l]=nums[r]
-#         l=l+1
-
-# print(nums)
 
