0b85435a8ab1a0daf6f38d340399a28078e67e37
hsscode <harshsengar28@gmail.com>
2024-12-07 16:01:42 +0530

    solution 69  https://leetcode.com/problems/sqrtx/description/?envType=study-plan-v2&envId=top-interview-150

---
+++ solution69.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/sqrtx/description/?envType=study-plan-v2&envId=top-interview-150
+
+# Sqrt(x)
+
+
+# Example 1:
+
+# Input: x = 4
+# Output: 2
+# Explanation: The square root of 4 is 2, so we return 2.
+
+
+x= 8
+
+a= x**0.5
+print(int(a))
